a604743645891c44a8f99328f795c7c4b90057c5
JustForFunDeveloper <tappler.andreas@gmail.com>
2020-10-25 10:35:26 +0100

    Add project files.

---
+++ ShootingWebAgent/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShootingWebAgent.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
